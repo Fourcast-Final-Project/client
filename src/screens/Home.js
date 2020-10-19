19131d9c131a0df53c2b158a481ca58b8dc21230
ajiYudhanto <aji.yudhanto@gmail.com>
2020-10-19 21:51:12 +0700

    npm i

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,7 @@ import CardComponent from '../components/CardComponent'
 import firebase from 'firebase'
 import database from '../config/firebase'
 import {getUserLocation} from '../store/actions/userActions'
+import publicIP from 'react-native-public-ip'
 
 export default function Home({navigation}) {
     const [data, setData] = useState('')
@@ -17,15 +18,22 @@ export default function Home({navigation}) {
     }
 
         useEffect(()=>{
-            // if (!firebase.apps.length) {
-            //     firebase.initializeApp(firebaseConfig);
-            //  }
-
-            // database.ref('location/002').orderByKey().on('value',snapshoot =>{
-            // // console.log(snapshoot.val())
-            // setData(snapshoot.val())
-            // })
-            dispatch(getUserLocation("jakarta"))
+            publicIP()
+            .then(ip => {    
+                console.log(ip);
+                return axios({
+                    method: 'get',
+                    url: `https://cors-anywhere.herokuapp.com/https://api.ip2location.com/v2/?package=WS24&ip=${ip}&format=json&key=LYDJRXN1GG`
+                })
+            })
+            .then(data => {
+                console.log(data.data.city_name)
+                dispatch(getUserLocation(data.data.city_name));
+            })
+            .catch(error => {
+                console.log(error);
+                // 'Unable to get IP address.'
+            });
         },[])
 console.log(location, '<<<<<< location')
     return (
