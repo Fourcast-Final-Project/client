11c97e638c981d647955b06c705a21f27a55ee0c
ajiYudhanto <aji.yudhanto@gmail.com>
2020-10-19 08:56:20 +0700

    change baseUrl on store

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,6 +1,6 @@
 import { SET_TOKEN, SET_USER, SET_SUBSCRIBED, SET_LOCATION } from './types';
 import axios from 'axios';
-const baseUrl = 'http://192.168.1.174:3000'
+const baseUrl = 'http://192.168.1.175:3000'
 
 export const setToken = (token) => {
   return {
