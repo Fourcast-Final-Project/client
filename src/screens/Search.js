bdade3b665b5e18a31f147de1d5e9336cb8a3aa8
ajiYudhanto <aji.yudhanto@gmail.com>
2020-10-20 15:20:24 +0700

    done filtering histry by city name

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { View, StyleSheet, Dimensions, TextInput, Text,ActivityIndicator } from 'react-native'
+import { View, StyleSheet, Dimensions, TextInput, Text,ActivityIndicator, Pressable, Modal } from 'react-native'
 import { SearchBar} from 'react-native-elements';
 import { useDispatch, useSelector } from 'react-redux'
 import SearchCard from '../components/SearchCard'
 import useDebounce from '../hooks/useDebounce' 
-import { searchLocation,setHistory } from '../store/actions/dataActions'
+import { searchLocation,setHistory, getByCity, getWeather } from '../store/actions/dataActions'
+import SearchByCity from '../components/SearchByCity';
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -12,14 +13,15 @@ const windowHeight = Dimensions.get("window").height;
 export default function Search({navigation}) {
     const dispatch = useDispatch()
     const [search, setSearch] = useState("")
+    const [city, setCity] = useState('')
     const delay = 100
     const debouncedSearchTerm = useDebounce(search, delay);
     const [isZero, setIsZero] = useState(true)
+    const [open, setOpen] = useState(false)
     let searchResults = useSelector(state => state.dataReducer.searchData)
-
+    let filteredLoc = useSelector(state => state.dataReducer.filteredByCity)
     
     useEffect(() => {
-        
         let newArr = []
         if (debouncedSearchTerm) {
                 dispatch(searchLocation(search))
@@ -30,9 +32,27 @@ export default function Search({navigation}) {
         [debouncedSearchTerm]
     );
 
+    useEffect(() => {
+        if (searchResults.length === 1) {
+            // console.log(searchResults, 'ini di if 1')
+            setCity(searchResults[0].city)
+        } else if (searchResults.length > 1) {
+            setCity('')
+        }
+    }, [searchResults])
+
     function handleOnChange (search) {
         setSearch(search)
         //console.log(search, 'searching')
+    }
+
+    function handleOnPress () {
+        dispatch(getByCity(city))
+        setOpen(true)
+    }
+
+    function falsingSetOpen () {
+        setOpen(false)
     }
 
     return (
@@ -55,18 +75,31 @@ export default function Search({navigation}) {
                         onChangeText={(event) => handleOnChange(event)}
                     />
                 </View>
-                    <Text>
-                        {/* {
-                            JSON.stringify(searchResults)
-                        } */}
-                    </Text>
+                <View style={ styles.search }>
+                    {
+                        city ? <Pressable onPress={() => handleOnPress()}>
+                            <Text  style={styles.textInput}>{ city }</Text>
+                        </Pressable> : <></>
+                    }
+                </View>
                     {
                         searchResults.map((location) => {
                             return <SearchCard location={ {location,navigation} }  key={ location.id } /> 
-                        }) 
-                        
+                        })  
                     }
-                {/* <CardComponent/> */}
+                { filteredLoc ? <Modal animationType="slide" transparent={false} visible={open}>
+                        {
+                            filteredLoc.map(location => {
+                                // return <Text>
+                                //     {
+                                //         JSON.stringify(location)
+                                //     }
+                                // </Text>
+                                return <SearchByCity falsingSetOpen={ falsingSetOpen } key={ location.id } id={ location.id } location={ location } getWeather={ getWeather } navigation={ navigation } />
+                            })
+                        }
+                    </Modal> : <></>
+                }
             </View>
         </>
     )
--- src/store/actions/dataActions.js
+++ src/store/actions/dataActions.js
@@ -1,4 +1,4 @@
-import { SET_DATA, SEARCH_DATA, SET_HISTORY } from './types';
+import { SET_DATA, SEARCH_DATA, SET_HISTORY, SET_FILTERED_LOCATIONS } from './types';
 import axios from 'axios';
 const baseUrl = 'http://192.168.1.177:3000'
 
@@ -16,10 +16,24 @@ export const setSearch = (payload) => {
   }
 }
 
+export const setHistory = (payload) => {
+  return {
+    type: SET_HISTORY,
+    payload
+  }
+}
+
+export const setFilteredLocations = (payload) => {
+  return {
+    type: SET_FILTERED_LOCATIONS,
+    payload
+  }
+}
+
 export const searchLocation = (place) => {
   console.log ("masuk userAction: ini search")
   return (dispatch) => {
-    fetch(`${baseUrl}/locations/find/${place}`)
+    fetch(`${baseUrl}/locations/search/${place}`)
       .then((res) => res.json())
       .then(({data}) => {
            console.log(data, 'INI DRI SEARCH LOC')
@@ -36,6 +50,7 @@ export const getHistory = (id) => {
   console.log ("~~~~~~~~~~~~~")
   console.log ("masuk get history")
   return (dispatch,getState) => {
+    dispatch(setHistory([]))
     const token = getState().usersReducer.token;
     fetch(`${baseUrl}/histories/${id}`, {
       method: 'GET',
@@ -55,10 +70,24 @@ export const getHistory = (id) => {
   }    
 }
 
-
-export const setHistory = (payload) => {
-  return {
-    type: SET_HISTORY,
-    payload
+export const getByCity = (cityName) => {
+  console.log(cityName, 'nama city cuy')
+  return (dispatch, getState) => {
+    const token = getState().usersReducer.token;
+    fetch(`${baseUrl}/locations/city/${cityName}`, {
+      method: 'GET',
+      headers: {
+        access_token: token
+      },
+      redirect: 'follow'
+    })
+      .then((res) => res.json())
+      .then((data) => {
+          console.log(data, 'INI DRI filtered loc')
+          dispatch(setFilteredLocations(data.data))
+      })
+      .catch((err) => {
+          console.log(err)
+      })
   }
 }
